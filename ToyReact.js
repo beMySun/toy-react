69657e1964e2b806a0c6a0f13038820856007777
Yongkang Yan <yongkang.yan@shopee.com>
2020-08-02 23:07:42 +0800

    fix: render component

--- ToyReact.js
+++ ToyReact.js
@@ -1,15 +1,49 @@
+class ElementWrapper {
+  constructor(type) {
+    this.root = document.createElement(type);
+  }
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value);
+  }
+  appendChild(vChild) {
+    vChild.mountTo(this.root);
+  }
+  mountTo(parent) {
+    parent.appendChild(this.root);
+  }
+}
+
+class TextWrapper {
+  constructor(content) {
+    this.root = document.createTextNode(content);
+  }
+  mountTo(parent) {
+    parent.appendChild(this.root);
+  }
+}
+
 export const ToyReact = {
-  createElement (type, attributes, ...children) {
-    let element = document.createElement(type);
-    for(let name in attributes) {
+  createElement(type, attributes, ...children) {
+    let element;
+    if(typeof type === 'string') {
+      element = new ElementWrapper(type);
+    } else {
+      element = new type;
+    }
+
+    // let element = document.createElement(type);
+    for (let name in attributes) {
       element.setAttribute(name, attributes[name]);
     }
-    for(let child of children) {
-      if(typeof child === "string") {
-        child = document.createTextNode(child);
+    for (let child of children) {
+      if (typeof child === 'string') {
+        child = new TextWrapper(child);
       }
       element.appendChild(child);
     }
     return element;
-  }
-}
+  },
+  render(vdom, root) {
+    vdom.mountTo(root);
+  },
+};
--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 import { ToyReact } from './ToyReact.js';
+class MyConponent {
+  render() {
+    return <div> render something.</div>;
+  }
+  setAttribute(name, value) {
+    this[name] = value;
+  }
+  mountTo(parent) {
+    let vdom = this.render();
+    vdom.mountTo(parent);
+  }
+}
 
-let a = <div name="a" id="ida">
-  <span>1</span>
-  <span>2</span>
-  <span>3</span>
-</div>;
+// let a = (
+//   <div name="a" id="ida">
+//     <span>1</span>
+//     <span>2</span>
+//     <span>4</span>
+//   </div>
+// );
 
-document.body.appendChild(a);
+let a = <MyConponent name="a" id="ida"></MyConponent>
+
+// document.body.appendChild(a);
+
+ToyReact.render(a, document.body);
 
 console.log(a);
