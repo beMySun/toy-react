45d160114ba1bb4eed1fc98733c98f8addde9893
Yongkang Yan <yongkang.yan@shopee.com>
2020-08-03 22:40:23 +0800

    fix: child type

--- ToyReact.js
+++ ToyReact.js
@@ -23,12 +23,18 @@ class TextWrapper {
 }
 
 export class Component {
+  constructor() {
+    this.children = [];
+  }
   setAttribute(name, value) {
     this[name] = value;
   }
   mountTo(parent) {
     let vdom = this.render();
     vdom.mountTo(parent);
+  }
+  appendChild(vChild) {
+    this.children.push(vChild);
   }
 }
 
@@ -45,12 +51,28 @@ export const ToyReact = {
       element.setAttribute(name, attributes[name]);
     }
 
-    for (let child of children) {
-      if (typeof child === 'string') {
-        child = new TextWrapper(child);
+    let insertChildren = (children) => {
+      for (let child of children) {
+        if (typeof child === 'object' && child instanceof Array) {
+          insertChildren(child);
+        } else {
+          if (
+            !(child instanceof Component) &&
+            !(child instanceof ElementWrapper) &&
+            !(child instanceof TextWrapper)
+          ) {
+            child = String(child);
+          }
+          if (typeof child === 'string') {
+            child = new TextWrapper(child);
+          }
+          element.appendChild(child);
+        }
       }
-      element.appendChild(child);
-    }
+    };
+
+    insertChildren(children);
+
     return element;
   },
 
--- main.js
+++ main.js
@@ -2,10 +2,17 @@ import { ToyReact, Component } from './ToyReact';
 
 class MyComponent extends Component {
   render() {
-    return <div> This is awsome ! </div>;
+    return (
+      <div> 
+        <div>This is awsome ! </div>
+        <div>
+          {this.children}
+        </div>
+      </div>
+    );
   }
 }
 
-const element = <MyComponent name="kk" />;
+const element = <MyComponent name="kk"> test </MyComponent>;
 
 ToyReact.render(element, document.getElementById('root'));
